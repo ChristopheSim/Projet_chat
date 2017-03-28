afaaabcd605235b23631952244e0a048c8f8ef56
Guillaume de Moffarts <Guillaume de Moffarts>
2017-03-28 16:03:16 +0200

    add run method to Server

--- cs.py
+++ cs.py
@@ -8,3 +8,13 @@ SERVERADDRESS = (socket.gethostname(),6000)
  	def __init__(self):
  		self.__s = socket.socket()
  		self.__s.bind(SERVERADDRESS)
+
+ 	def run(self):
+ 		self.__s.listen()
+ 		while True:
+ 			client, address = self.__s.accept()
+ 			try:
+ 				print (client,address)
+ 				client.close()
+ 			except OSError:
+ 				print("erreur lors de la requète") 
