0785827ef01f8add9d3b72b4de7e4697ee20121e
Guillaume de Moffarts <Guillaume de Moffarts>
2017-03-28 17:02:23 +0200

    add temporary startup code

--- cs.py
+++ cs.py
@@ -9,13 +9,14 @@ class ChatServer:
  	def __init__(self):
  		self.__s = socket.socket()
  		self.__s.bind(SERVERADDRESS)
+ 		self._connected = []
 
  	def run(self):
  		self.__s.listen()
  		while True:
  			print("hellow")
  			client, address = self.__s.accept()
-
+ 			self._connected.append((client,address)) 
  			try:
  				print(client,address)
  				client.close()
@@ -23,7 +24,7 @@ class ChatServer:
  				print("erreur lors de la requète") 
 
 class ChatClient:
-	def __init__(self,command):
+	def __init__(self,command = '' ):
 		self.__data = [int(x) for x in command]
 		self.__s = socket.socket()
 
@@ -34,4 +35,8 @@ class ChatClient:
 		except OSError:
 			print("Problème lors de la connection au server")
 
-ChatServer().run()
+if __name__ == '__main__':
+	if len(sys.argv) == 2 and sys.argv[1] == 'server':
+		ChatServer().run()
+	elif len(sys.argv) > 2 and sys.argv[1] == 'client':
+		ChatClient().run()
