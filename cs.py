34226176e00f8bf175de71f2ba8952597cdaa9d8
Guillaume de Moffarts <Guillaume de Moffarts>
2017-03-28 15:41:31 +0200

    initiate ChatServer class

--- cs.py
+++ cs.py
@@ -2,4 +2,9 @@
  import socket 
  import struct
  import sys
- 
+
+SERVERADDRESS = (socket.gethostname(),6000)
+ class ChatServer:
+ 	def __init__(self):
+ 		self.__s = socket.socket()
+ 		self.__s.bind(SERVERADDRESS)
